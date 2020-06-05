07ab3c79be72684d0b3ffa4e086eb5ffcf5b0362
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-05 12:24:36 -0400

    Part 1 Design

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        public MenuItem[] MenuItems { get; set; }
+        public string LastUpdated { get; set; }
+        
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool New { get; set; }
+
+
+    }
+}
